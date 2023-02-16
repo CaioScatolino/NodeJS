1057c602fc0083b9c8b729a8c479815cfec09144
Caio Scatolino <66438920+CaioScatolino@users.noreply.github.com>
2023-02-16 12:43:06 -0300

    Atualizando aulas

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -2,6 +2,7 @@
 // ESModules => import/export
 
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 // Criar um usuário (name, email, senha)
 
@@ -31,30 +32,19 @@ const server = http.createServer(async (req, res) => {
 
     const { method, url } = req
 
-    const buffers = []
 
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
-
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-    }
-   
+    await json(req, res)
 
     if (method == 'GET' && url == '/users') {
 
         //Early return
-        return res.
-            setHeader('Content-Type', 'application/json') // FORMATOU O JSON PARA VISUALIZAÇÃO
+        return res
             .end(JSON.stringify(users))
     }
 
     if (method == 'POST' && url == '/users') {
 
-        const {name, email} = req.body
+        const { name, email } = req.body
 
         users.push({
             id: 1,
---
+++ aulas/01-fundamentos-nodejs/streams/buffer.js
@@ -0,0 +1,6 @@
+// É uma representação de um espaço na memória do computador para transitar dados rapidamente
+// Guarda de maneira binária - hexadecimal
+
+const buf = Buffer.from('ok')
+
+console.log(buf.toJSON())
