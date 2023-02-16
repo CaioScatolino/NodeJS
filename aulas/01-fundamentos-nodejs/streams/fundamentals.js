27c2f16a6664a85f8ee2466ab06a8a6d0ab78bdd
Caio Scatolino <66438920+CaioScatolino@users.noreply.github.com>
2023-02-16 09:04:07 -0300

    Atualizando aulas

--- aulas/01-fundamentos-nodejs/streams/fundamentals.js
+++ aulas/01-fundamentos-nodejs/streams/fundamentals.js
@@ -8,7 +8,7 @@
 // process.stdin
 //     .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
 
@@ -29,5 +29,22 @@ class OneToHundredStream extends Readable {
 
 }
 
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed =  Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(transformed))) //
+        // Teve que usar o buffer para não dar erro, usou buffer para transicionar entre streams 
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+
 new OneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
